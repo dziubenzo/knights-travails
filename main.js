5e22431561c945de8ba3d7e4327de1ba2f225812
dziubenzo <dziubenzo@gmail.com>
2024-01-04 12:18:21 +0100

    Clean up code
    This covers adding comments, deleting experimental commented-out
    lines and building the knightMoves() function that outputs the
    full path. I also decided to use the BOARD_SIZE constant inside
    functions instead of passing it as a function parameter.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Global constant for setting grid size
 const BOARD_SIZE = 8;
 
 class Tree {
@@ -11,7 +12,10 @@ class Tree {
     }
   }
 
-  createBranches(tree = this, endingSquare, queue = []) {
+  // Find the parent of endingSquare through creating a recursive BFS tree
+  // I haven't been able to return all parents up to startingSquare (used an additional method for that), but I think this is possible
+  findParent(tree, endingSquare, queue = []) {
+    // Base case: the value property of the currently explored tree is equal to endingSquare
     if (
       tree.value[0] === endingSquare[0] &&
       tree.value[1] === endingSquare[1]
@@ -24,52 +28,37 @@ class Tree {
       }
       const parent = tree.value;
       const value = tree[`branch${branch}`];
-      const combinations = findMoves(value, BOARD_SIZE);
+      const combinations = findMoves(value);
       tree[`branch${branch}`] = new Tree(parent, value, combinations);
-      // Make sure only unique values are added to the queue
+      // Make sure only unique values are added to the queue (no need to explore identical trees over and over again)
+      // This has a HUGE impact on queue size
       if (!isFound(queue, tree[`branch${branch}`].value)) {
         queue.push(tree[`branch${branch}`]);
       }
     }
-    const parent = this.createBranches(queue.shift(), endingSquare, queue);
-    return parent;
+    return this.findParent(queue.shift(), endingSquare, queue);
   }
 
+  // Find all squares between startingSquare and endingSquare by calling the findParent() method until it returns startingSquare
+  // Return an array of moves in the correct order
   static findShortestPath(startingSquare, endingSquare) {
     let path = [];
     let square = endingSquare;
-    path.push(square);
     while (true) {
-      if (square[0] === startingSquare[0] && square[1] === startingSquare[1]) {
-        return path;
-      }
-      let tree = new Tree(null, startingSquare, findMoves(startingSquare, BOARD_SIZE))
-      square = tree.createBranches(tree, square);
       path.push(square);
-    };
-  }
-}
-
-// Create the board as an array of coordinates
-function createBoard(size) {
-  let array = [];
-  let row = 0;
-  let column = 0;
-  while (array.length < size * size) {
-    array.push([row, column]);
-    if (column === size - 1) {
-      column = 0;
-      row++;
-    } else {
-      column++;
+      if (square[0] === startingSquare[0] && square[1] === startingSquare[1]) {
+        return path.reverse();
+      }
+      let tree = new Tree(null, startingSquare, findMoves(startingSquare));
+      square = tree.findParent(tree, square);
     }
   }
-  return array;
 }
 
 // Find out legal knight moves for a given square
-function findMoves(square, size) {
+function findMoves(square) {
   // Establish all possible combinations clockwise
+  // Changing the order below is what I believe leads to discovering other shortest paths
   const combinations = [
     [2, 1],
     [1, 2],
@@ -85,7 +74,12 @@ function findMoves(square, size) {
   for (const combination of combinations) {
     let row = square[0] + combination[0];
     let column = square[1] + combination[1];
-    if (row > size - 1 || row < 0 || column > size - 1 || column < 0) {
+    if (
+      row > BOARD_SIZE - 1 ||
+      row < 0 ||
+      column > BOARD_SIZE - 1 ||
+      column < 0
+    ) {
       continue;
     }
     moves.push([row, column]);
@@ -93,7 +87,7 @@ function findMoves(square, size) {
   return moves;
 }
 
-// Check an array of trees for the presence of a square
+// Check a queue of trees for the presence of a square
 function isFound(array, squareToFind) {
   if (
     array.find(
@@ -106,62 +100,19 @@ function isFound(array, squareToFind) {
   return true;
 }
 
-function knightMoves(startingSquare, endingSquare, size) {
-  let queue = findMoves(startingSquare, size);
-  let visitedCombinations = [];
-  let array = [];
-  while (queue.length > 0) {
-    if (isFound(queue, endingSquare)) {
-      console.log(visitedCombinations);
-      return 'done';
-      // let traverseBack = endingSquare;
-      // array.push(endingSquare);
-      // console.log(visitedCombinations);
-      // while (
-      //   traverseBack[0] !== startingSquare[0] ||
-      //   traverseBack[1] !== startingSquare[1]
-      // ) {
-      //   const combinations = findMoves(traverseBack, size);
-      //   for (const combination of combinations) {
-      //     if (
-      //       isFound(visitedCombinations, combination) ||
-      //       (combination[0] === startingSquare[0] &&
-      //         combination[1] === startingSquare[1])
-      //     ) {
-      //       traverseBack = combination;
-      //       const index = visitedCombinations.indexOf(combination);
-      //       visitedCombinations.splice(index, 1);
-      //       if (!isFound(array, traverseBack)) {
-      //         array.push(traverseBack);
-      //       }
-      //       break;
-      //     }
-      //   }
-      // }
-      // queue = [];
-      // return array;
-    }
-    const currentSquare = queue.shift();
-    visitedCombinations.push(currentSquare);
-    const combinations = findMoves(currentSquare, size);
-    // console.log(new Node(startingSquare, currentSquare, combinations));
-    for (const combination of combinations) {
-      if (!isFound(queue, combination)) {
-        queue.push(combination);
-      }
-      // queue.push(combination);
-    }
+function knightMoves(startingSquare, endingSquare) {
+  const moves = Tree.findShortestPath(startingSquare, endingSquare);
+  if (moves.length - 1 === 0) {
+    console.log(
+      'Starting square is the same as ending square. No moves required!'
+    );
+    return;
   }
-  return array;
+  console.log(`Done! It took ${moves.length - 1} moves.`);
+  console.log('The path goes like this:');
+  for (const move of moves) {
+    console.log(move);
+  }
 }
 
-const board = createBoard(BOARD_SIZE);
-// console.log(board);
-// const moves = findMoves([0, 6], BOARD_SIZE);
-// console.log(moves);
-
-const startingSquare = [0, 0];
-const endingSquare = [3, 3];
-console.log(Tree.findShortestPath(startingSquare, endingSquare));
-
-// console.log(knightMoves([0, 0], [7, 7], BOARD_SIZE));
+knightMoves([0, 0], [7, 7]);
